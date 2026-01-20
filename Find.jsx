f199953dda0da304d3948319308307eec87dfea7
siddhi <siddhi.kotgire@pzwind.com>
2026-01-20 15:02:57 +0530

    Updated Scroll

---
+++ Find.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Find = () => {
+    return (
+        <>
+            <div>Updated find on branch intern-4</div>
+            <div>Find</div>
+        </>
+
+    )
+}
+
+export default Find
